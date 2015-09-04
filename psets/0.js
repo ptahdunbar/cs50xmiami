94dbf328ec45a3fd764e2218ba32a6f9dd9df503
Ptah Dunbar <yarr@piratedunbar.com>
2015-09-03 20:07:04 -0400

    Finished first pset

--- psets/0.js
+++ psets/0.js
@@ -11,3 +11,14 @@ Why pay a fortune teller when you can just program your fortune yourself?
 */
 
 // write your solution here...
+
+var numChildren = 2;
+
+var partnerName = "Taylor Swift";
+
+var geoLocal = "Miami";
+
+var jobTitle = "Hacker";
+
+console.log("You will be a "+ jobTitle+ " in " + geoLocal + ", and married\
+ to " + partnerName + " with " + numChildren+ " kids.");
